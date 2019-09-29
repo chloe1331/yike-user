8ad78da8b106732ac865f8970919158adf0e302b
zhulingxiao <zhulingxiao@yiwei.com>
2019-09-29 22:26:01 +0800

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -188,11 +188,11 @@ export default class Index extends Component {
                     this.moveOptions.x = this.moveOptions.x + 1;
                 }
                 if (e.keyCode == 69) {
-                    this.moveOptions.size = this.moveOptions.size + 0.2;
+                    this.moveOptions.size = this.moveOptions.size + 0.2 / 100;
                     this.forceUpdate();
                 }
                 if (e.keyCode == 82) {
-                    this.moveOptions.size = this.moveOptions.size - 0.2;
+                    this.moveOptions.size = this.moveOptions.size - 0.2 / 100;
                     this.forceUpdate();
                 }
                 this.getCanvas();
