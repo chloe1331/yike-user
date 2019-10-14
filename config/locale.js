c784df1ae5da7c9b71bc7dcf9ff02ae45d71c38a
zhulingxiao <zhulingxiao@yiwei.com>
2019-10-14 17:43:20 +0800

    update

--- config/locale.js
+++ config/locale.js
@@ -1,18 +1,18 @@
 module.exports = {
     development: {
         url: {
-            api: '//localhost:7001',
-            cdn: 'http://pxyn2mfh8.bkt.clouddn.com/',
-            cdnStatic: 'http://pxynkk8s9.bkt.clouddn.com/',
-            cdnUser: 'http://pya5657b1.bkt.clouddn.com/'
+            api: '//api.yikebb.com',
+            cdn: 'http://cdn-upload.yikebb.com/',
+            cdnStatic: 'http://cdn-static.yikebb.com/',
+            cdnUser: 'http://cdn-user.yikebb.com/'
         }
     },
     production: {
         url: {
-            api: '//47.99.38.214:7001',
-            cdn: 'http://pxyn2mfh8.bkt.clouddn.com/',
-            cdnStatic: 'http://pxynkk8s9.bkt.clouddn.com/',
-            cdnUser: 'http://pya5657b1.bkt.clouddn.com/'
+            api: '//api.yikebb.com',
+            cdn: 'http://cdn-upload.yikebb.com/',
+            cdnStatic: 'http://cdn-static.yikebb.com/',
+            cdnUser: 'http://cdn-user.yikebb.com/'
         }
     }
 };
