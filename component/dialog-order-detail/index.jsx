4cdabd5f5cbcfc02742cad34e77c2821dd6454d7
zhulingxiao <zhulingxiao@yiwei.com>
2019-11-15 11:10:48 +0800

    update

--- component/dialog-order-detail/index.jsx
+++ component/dialog-order-detail/index.jsx
@@ -2,35 +2,21 @@ import { Component } from 'react';
 import { Modal } from 'antd';
 import PropTypes from 'prop-types';
 
-import { MServer } from 'public/utils';
 import Dialog from '../dialog';
 import OrderList from '../order-list';
 
 class DialogOrderDetail extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: []
-        };
-    }
-
-    componentDidMount() {
-        const { order_sn } = this.props;
-        MServer.get('/order/userlist', {
-            order_sn
-        }).then(res => {
-            this.setState({
-                list: res.errcode == 0 ? res.data : []
-            });
-        });
-    }
-
     render() {
-        const { list } = this.state;
+        const { order_sn } = this.props;
 
         return (
             <Modal {...this.props} title="订单详情" width={980} footer={false} centered>
-                <OrderList list={list}></OrderList>
+                <OrderList
+                    action='/order/userlist'
+                    condition={{
+                        order_sn,
+                    }}
+                ></OrderList>
             </Modal>
         );
     }
